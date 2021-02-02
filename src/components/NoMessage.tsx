3348d2d7b56465292ffea043e9ed328aa08d8e98
leularia <leulsegedtariku@gmail.com>
2021-02-02 22:38:13 +0300

    🐛 no message component

---
+++ src/components/NoMessage.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Zap } from 'react-feather';
+
+const NoMessage = () => {
+  return (
+    <>
+      <div className="no-message-container">
+        <h3>Click a chat to start messaging <Zap /></h3>
+      </div>
+
+      <style>
+        {`
+					.no-message-container {
+						width: 100%;
+						height: 100%;
+						display: flex;
+						align-items: center;
+						justify-content: center;
+					}
+					.no-message-container > h3 {
+						margin-top: -15vh;
+					}
+				`}
+      </style>
+    </>
+  );
+};
+
+export default NoMessage;
